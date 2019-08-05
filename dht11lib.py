c0f133ea0205bfd5c7fe8b9c2462f7dfab761955
ironicoerror <rogalla.tobias@googlemail.com>
2019-08-05 22:08:00 +0200

    set up dht11 with lib for flaskserver and simple LED tasks

---
+++ dht11lib.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import Adafruit_DHT
+import time
+
+sensor = Adafruit_DHT.DHT11
+
+def __init__():
+    pass
+
+def define_pin():
+    pin = int(input("Type pin for the data port here :"))
+    return pin
+
+def get_data(sensor, pin):
+    times = 0
+    while times < 10:
+        data = hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+        print(data)
+        times += 1
+        time.sleep(1)
+    return 1
