1d948868ad27d042b0190bcc92c9824c627ddeb2
ironicoerror <rogalla.tobias@googlemail.com>
2019-08-17 16:48:00 +0200

    try except finally added to be sure that the PIN setup will be resetted after the script is canceled via KeyInterrupt

--- led/transistor_led.py
+++ led/transistor_led.py
@@ -5,12 +5,19 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 BASEPIN = 8
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BASEPIN, GPIO.OUT)
-
-print("LED ON")
-GPIO.output(BASEPIN, GPIO.HIGH)
-sleep(1)
-print("LED  OFF")
-GPIO.output(BASEPIN, GPIO.LOW)
-GPIO.cleanup()
+try:
+    while True:
+        print("LED ON")
+        GPIO.output(BASEPIN, GPIO.HIGH)
+        sleep(1)
+        print("LED  OFF")
+        GPIO.output(BASEPIN, GPIO.LOW)
+        sleep(1)
+except KeyboardInterrupt:
+    print("Programm canceled through KeyInterrupt.")
+finally:
+    print("Cleaning up...")
+    GPIO.cleanup()
+print("Finished script!")
