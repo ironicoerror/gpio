84a796882f6a695e84e269beab8b99041db85f8b
ironicoerror <rogalla.tobias@googlemail.com>
2019-08-18 23:20:37 +0200

    Added event detect for lesser CPU usage

--- led/led_switch.py
+++ led/led_switch.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 """Turning an LED on as long as the switch is pressed"""
 
-from time import sleep
 import RPi.GPIO as GPIO
 
-OUTPIN = 18
+OUTPIN = 15
 SWITCHPIN = 21
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(OUTPIN, GPIO.OUT)
 GPIO.setup(SWITCHPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+status = True
+def switch_led_status(channel):
+    """detects if the LED is on or off and turns it into the complement"""
+    print("Rising signal on pin " + str(channel))
+    global status
+    status = not status
+    GPIO.output(OUTPIN, status)
 
-
-while True:
-    SWITCHVALUE = GPIO.input(SWITCHPIN)
-    if  SWITCHVALUE != 0:
-        print("LED ON")
-        GPIO.output(OUTPIN, GPIO.HIGH)
-        sleep(0.5)
-    else:
-        GPIO.output(OUTPIN, GPIO.LOW)
-GPIO.cleanup()
+try:
+    GPIO.add_event_detect(SWITCHPIN, GPIO.RISING,\
+         callback=switch_led_status, bouncetime=200)
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Programm canceled through KeyInterrupt.")
+finally:
+    print("Cleaning up...")
+    GPIO.cleanup()
+print("Finished script!")
